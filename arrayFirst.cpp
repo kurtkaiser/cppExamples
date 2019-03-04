a583384b20d56adc990cddaae1597f5aed579fa9
Kurt Kaiser <kurtbkaiser@gmail.com>
2019-03-03 20:33:48 -0500

    Create arrayFirst.cpp

---
+++ arrayFirst.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+ 
+int
+main () 
+{
+short numbers[] = {10, 20, 30, 40 , 50};
+
+ cout << "The first element: ";
+ cout << *numbers << endl; 
+  
+return 0;
+
+}
